ab0393a9597979a613ebb50bf3515db1298bd300
firebie <firebie@gmail.com>
2013-09-29 16:23:40 +0300

    DlgSelectEx: a copy of DlgSelect from Максим Русов (Max.Rusov@gmail.com)

---
+++ DlgSelectEx/DlgSelectEx.cpp
@@ -0,0 +1,92 @@
+#include <plugin.hpp>
+#include <initguid.h>
+
+#include "guid.hpp"
+#include "version.hpp"
+
+static struct PluginStartupInfo Info;
+
+void WINAPI GetGlobalInfoW(struct GlobalInfo *Info)
+{
+  Info->StructSize=sizeof(GlobalInfo);
+  Info->MinFarVersion=FARMANAGERVERSION;
+  Info->Version=PLUGIN_VERSION;
+  Info->Guid=MainGuid;
+  Info->Title=PLUGIN_NAME;
+  Info->Description=PLUGIN_DESC;
+  Info->Author=PLUGIN_AUTHOR;
+}
+
+void WINAPI SetStartupInfoW(const struct PluginStartupInfo *psi)
+{
+  Info=*psi;
+}
+
+void WINAPI GetPluginInfoW(struct PluginInfo *Info)
+{
+  Info->StructSize = sizeof(*Info);
+  Info->Flags      = PF_PRELOAD;
+}
+
+size_t GetTextLen(HANDLE hDlg, intptr_t Id)
+{
+  size_t nLen = (size_t)Info.SendDlgMessage(hDlg, DM_GETTEXT, Id, nullptr);
+
+  if (nLen > 0)
+  {
+    FarDialogItemData ID = {sizeof(ID)};
+    ID.PtrLength = nLen + 1;
+    ID.PtrData   = new wchar_t[nLen + 1];
+    Info.SendDlgMessage(hDlg, DM_GETTEXT, Id, &ID);
+    wchar_t* str = Info.FSF->Trim(ID.PtrData);
+    nLen = wcslen(str);
+
+    delete[] ID.PtrData;
+  }
+
+  return nLen;
+}
+
+void ProcessFocus(intptr_t Msg, HANDLE hDlg, intptr_t Id)
+{
+  if (Msg == DN_GOTFOCUS || Msg == DN_KILLFOCUS)
+  {
+    FarDialogItem DI;
+    Info.SendDlgMessage(hDlg, DM_GETDLGITEMSHORT, Id, &DI);
+    if ((DI.Type == DI_EDIT || DI.Type == DI_PSWEDIT || DI.Type == DI_FIXEDIT)
+      && (DI.Flags & DIF_EDITOR) == 0)
+    {
+      EditorSelect ES = {sizeof(ES), 0, 0, 0, 0, 0};
+
+      size_t nLen = 0;
+      if (Msg == DN_GOTFOCUS)
+      {
+        if (DI.Type == DI_FIXEDIT)
+          nLen = GetTextLen(hDlg, Id);
+        else
+          nLen = (size_t)Info.SendDlgMessage(hDlg, DM_GETTEXT, Id, nullptr);
+
+        ES.BlockType   = BTYPE_STREAM;
+        ES.BlockWidth  = nLen;
+        ES.BlockHeight = 1;
+
+        Info.SendDlgMessage(hDlg, DM_SETSELECTION, Id, &ES);
+
+        COORD Coord = { (SHORT)nLen, 0 };
+        Info.SendDlgMessage(hDlg, DM_SETCURSORPOS, Id, &Coord);
+      }
+      else
+      {
+        Info.SendDlgMessage(hDlg, DM_SETSELECTION, Id, &ES);
+      }
+    }
+  }
+}
+
+intptr_t WINAPI ProcessDialogEventW(const struct ProcessDialogEventInfo *Info)
+{
+  if (Info->Event == DE_DEFDLGPROCINIT)
+    ProcessFocus(Info->Param->Msg, Info->Param->hDlg, Info->Param->Param1);
+
+  return 0;
+}
---
+++ DlgSelectEx/vc_crt_fix_impl.cpp
@@ -0,0 +1,82 @@
+/*
+vc_crt_fix_impl.cpp
+
+Workaround for Visual C++ CRT incompatibility with old Windows versions
+*/
+/*
+Copyright © 2011 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <windows.h>
+
+// EncodePointer / DecodePointer (VC2010)
+static PVOID WINAPI ReturnSamePointer(PVOID Ptr)
+{
+	return Ptr;
+}
+
+template<class T>
+static PVOID WINAPI Wrapper(PVOID Ptr)
+{
+	typedef PVOID (WINAPI *PointerFunction)(PVOID);
+	static PVOID FunctionAddress = GetProcAddress(GetModuleHandleW(L"kernel32"), T::get());
+	static PointerFunction ProcessPointer = FunctionAddress? reinterpret_cast<PointerFunction>(FunctionAddress) : ReturnSamePointer;
+	return ProcessPointer(Ptr);
+}
+
+#define STRING_OBJECT(name, string) struct name {static const char* get() {return string;}}
+extern "C"
+{
+	PVOID WINAPI EncodePointerWrapper(PVOID Ptr) {STRING_OBJECT(EncodePointerName, "EncodePointer"); return Wrapper<EncodePointerName>(Ptr);}
+	PVOID WINAPI DecodePointerWrapper(PVOID Ptr) {STRING_OBJECT(DecodePointerName, "DecodePointer"); return Wrapper<DecodePointerName>(Ptr);}
+}
+
+
+// GetModuleHandleExW (VC2012)
+static BOOL WINAPI GetModuleHandleExWImpl(DWORD Flags, LPCWSTR ModuleName, HMODULE *Module)
+{
+	BOOL Result = FALSE;
+	if (Flags)
+	{
+		SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+	}
+	else
+	{
+		if ((*Module = GetModuleHandleW(ModuleName)))
+		{
+			Result = TRUE;
+		}
+	}
+	return Result;
+}
+
+extern "C" BOOL WINAPI GetModuleHandleExWWrapper(DWORD Flags, LPCWSTR ModuleName, HMODULE *Module)
+{
+	typedef BOOL (WINAPI *GetModuleHandleExWFunction)(DWORD, LPCWSTR, HMODULE*);
+	static PVOID FunctionAddress = GetProcAddress(GetModuleHandleW(L"kernel32"), "GetModuleHandleExW");
+	static GetModuleHandleExWFunction ProcessGetModuleHandleExW = FunctionAddress? reinterpret_cast<GetModuleHandleExWFunction>(FunctionAddress) : GetModuleHandleExWImpl;
+	return ProcessGetModuleHandleExW(Flags, ModuleName, Module);
+}
